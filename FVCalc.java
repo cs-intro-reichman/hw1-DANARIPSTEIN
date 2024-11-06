7848c114c8f2714a6262fd9ce5e878e668745f78
DANARIPSTEIN <dana.ripstein@post.runi.ac.il>
2024-11-06 21:08:57 +0200

    complate

--- FVCalc.java
+++ FVCalc.java
@@ -3,10 +3,9 @@ public class FVCalc {
 	public static void main(String[] args) {
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
-		rate = rate / 100;
 		int n = Integer.parseInt(args[2]);
 		double ftureValue;
-		ftureValue = currentValue * Math.pow(1 + rate, n);
+		ftureValue = currentValue * Math.pow(1 + (rate/100), n);
 		System.out.println("After " + n + " years, a $" + currentValue + " saved at " +
 				rate + "% will yield $" + (int) ftureValue);
 	}
