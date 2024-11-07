c789cea8cf927a8ba0a60715bb310cd67dbe231b
DANARIPSTEIN <dana.ripstein@post.runi.ac.il>
2024-11-07 16:47:48 +0200

    complate

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,40 +1,25 @@
 // Represents the hh:mm time format using an AM/PM format. 
+
+import javax.print.DocFlavor.STRING;
+
 public class TimeFormat {
 	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
-		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
-		// It then concatenates the resulting string with the rightmost hour-digit,
-		// and then uses parseInt to cast the resulting string as an int.
+		String name1 = args[0];
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
-
-		//*  public static void main(String[] args) {
-       
-		int hours = Integer.parseInt(args[0]);
-        String s = args[1];
-        int minutes = Integer.parseInt(args[2]);
-        if (hours > 12) {
-            System.out.print(hours - 12 + ":" + minutes + " pm");
-        }
+        String amOrPm = " AM";
+		if (hours >= 12){
+			amOrPm = " PM";
+			}
+		if (hours > 12) {
+           hours = hours - 12;
+		}
         if (hours<10) {
-            System.out.print(hours % 10 + ":" + minutes + " am");
+            hours = hours % 10;
         }
-		public static void main(String[] args) {
-			int hours = Integer.parseInt(args[0]);
-			String s = args[1];
-			int minutes = Integer.parseInt(args[2]);
-			if (hours > 12) {
-				System.out.print(hours - 12 + ":" + minutes + " pm");
-			}
-			if (hours<10) {
-				System.out.print(hours % 10 + ":" + minutes + " am");
-			}
-			   
+		if (minutes<10) {
+		System.out.println(hours + ":0" + minutes + amOrPm);
+        } else {
+		System.out.println(hours + ":" + minutes + amOrPm);}
 	}
 }
